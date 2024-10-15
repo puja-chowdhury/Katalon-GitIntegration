f9c3d06b2bce4de83fbb8f5f79b3b1af16a87b8a
PujaChowdhury <PujaChowdhury@LINKOLNOV23-142>
2024-10-15 15:32:42 +0530

    Created before testcase testlisteners

--- Scripts/Login TestCase/Script1728977783575.groovy
+++ Scripts/Login TestCase/Script1728977783575.groovy
@@ -17,9 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
 
-WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
 
 WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Make Appointment'))
 
